b02048fbb0c03a1120627501a0fbbee2ea530d0a
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-03 17:55:39 -0700

    The bot can now join the voice channel.

---
+++ commands/join.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'join',
+    aliases: ['leave'], 
+    cooldown: 0,
+    description: "Join's the user's channel.",
+    async execute(client, message, cmd, args, Discord){
+
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
+        const permissions = voice_channel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
+        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+
+        if(cmd === 'join'){
+            const connection = await voice_channel.join();
+            message.react('👌');
+
+            const joinEmbed = new Discord.MessageEmbed()
+        .setColor('#1F3548')
+        .setDescription('I joined your voice channel!')
+
+        message.channel.send(joinEmbed)
+
+        }
+          
+        else if(cmd === 'leave'){
+            const connection = await voice_channel.leave();
+            message.react('👋');
+
+            const leaveEmbed = new Discord.MessageEmbed()
+        .setColor('#FF0051')
+        .setDescription('I left your voice channel!')
+
+        message.channel.send(leaveEmbed)
+            
+        }
+
+    }
+
+}
