6d977a71aa3572c27743ffa3882b79d8f5334f8d
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-03 21:59:37 -0700

    Update description.js

--- commands/description.js
+++ commands/description.js
@@ -1,22 +1,20 @@
 module.exports = {
     name: 'description',
-    description: "Sends and Embed of the Description.",
+    description: "Sends an Embed of the Description.",
     execute(client, message, cmd, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#1F3548')
         .setTitle('Description')
-        .setURL('https://www.xbftw.com')
+        .setURL('https://github.com/XBFTW/XBFTW-BOT')
         .setDescription('This bot was made by XBFTW#7381, if you have any questions or concerns about the functionality or have any ideas to add to the bot, feel free to contact me.')
         .addFields(
-            {name: 'Rule 1', value: 'Be nice'}
-            
+            {name: 'Check out my website:', value: 'https://xbftw.com'}
             
             )
         .setImage('https://i.imgur.com/kbUYO6k.png')
-        .setFooter('TEST')
+        .setFooter('HEY!')
 
         message.channel.send(newEmbed)
     }
-
 
 }
