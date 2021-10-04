14911aab7febf985dfb58c25d683ff56bbc6110b
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-03 21:53:08 -0700

    Added try/catch statements.

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,24 @@ module.exports = {
     name: 'ping',
     description: "It's Ping Pong!",
     execute(client, message, cmd, args, Discord){
-        message.channel.send('Pong!');
 
+        function catchErr (err, message) {
 
-        
+            require('dotenv').config();
+
+            client.users.cache.get(`${process.env.OWNER}`).send('The bot crashed, the crash report: ```' + err + '```')
+            console.log("The bot has just prevented a crash, the report has been sent to XBFTW")
+            message.channel.send('The bot just prevented a crash, the report has been sent to XBFTW');
+
+          }
+
+        try{
+            message.channel.send('Pong!');
+        }
+        catch (err) {
+           catchErr(err, message);
+        }
+        //dont copy past this part if you only want the try/catch
     }
-
 
 }
