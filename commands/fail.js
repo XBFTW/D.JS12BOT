8ff4cb99d1f09c9ae0f8f26eccac05353b18759c
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-20 20:22:26 -0700

    Removed function.

--- commands/fail.js
+++ commands/fail.js
@@ -1,32 +1,16 @@
 module.exports = {
-    name: 'fail',
-    description: "Causes the discord bot to crash",
-    execute(client, message, cmd, args, Discord){
+  name: 'fail',
+  description: "Causes the discord bot to crash",
+  execute(client, message, cmd, args, Discord, catchErr){
 
-//This command does not really have any real purpose, I just use it to test my crash prevention.
-
-function catchErr (err, message) {
-
-    require('dotenv').config();
-
-    client.users.cache.get(`${process.env.OWNER}`).send('The bot crashed, the crash report: ```' + err + '```')
-    console.log("The bot has just prevented a crash, the report has been sent to XBFTW")
-    message.channel.send('The bot just prevented a crash, the report has been sent to XBFTW');
-
-  }
-
-  try{
-
-//Normally code would go in this area, but for this command It purposely is not defined so it causes a crash. 
+try{
+//Put code here
 thisIsntDefined
-
-  }
-  catch (err) {
-    catchErr(err, message);
-  }
-
-
+}
+catch (err) {
+  catchErr(err, message);
+}
 //dont copy past this part
-    }
+  }
 
 }
