2579c44950a5c94d3bb3bdd4a89c3ca685f15a53
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-20 20:20:59 -0700

    Added Error Catching
    Added catchErr function to message.js so I could make the command files smaller.

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,4 +1,5 @@
 const config = require('../../config.json');
+require('dotenv').config();
 
 module.exports = (Discord, client, message) => {
     const prefix = config.prefix;
@@ -8,10 +9,16 @@ module.exports = (Discord, client, message) => {
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+    //Error catching
+    function catchErr (err, message) {
+        require('dotenv').config();
+        client.users.cache.get(`${process.env.OWNER}`).send('The bot crashed, the crash report: ```' + err + '```')
+        console.log("The bot has just prevented a crash, the report has been sent to XBFTW")
+        message.channel.send('The bot just prevented a crash, the report has been sent to XBFTW');
+      }
 
-    if(command) command.execute(client, message, cmd, args, Discord);
+    
+    if(command) command.execute(client, message, cmd, args, Discord, catchErr);
 
 }
-
-
 //This is to check the messages for commands
