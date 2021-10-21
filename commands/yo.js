2838d3afecdad59391c63ec306706d5102904424
XBFTW <74849594+XBFTW@users.noreply.github.com>
2021-10-20 20:23:52 -0700

    Removed function.

--- commands/yo.js
+++ commands/yo.js
@@ -1,29 +1,16 @@
 module.exports = {
     name: 'yo',
     description: "Literally just responds with Yo",
-    execute(client, message, cmd, args, Discord){
+    execute(client, message, cmd, args, Discord, catchErr){
 
-        function catchErr (err, message) {
-
-            require('dotenv').config();
-
-            client.users.cache.get(`${process.env.OWNER}`).send('The bot crashed, the crash report: ```' + err + '```')
-            console.log("The bot has just prevented a crash, the report has been sent to XBFTW")
-            message.channel.send('The bot just prevented a crash, the report has been sent to XBFTW');
-
-          }
-          
           try{
 //put what you want it to execute here
         message.channel.send('Yo!');
-
           }
           catch (err) {
             catchErr(err, message);
           }
                   
 //Don't copy past this point if you only seek for the try/cath statement.
-
-    }
-
+}
 }
